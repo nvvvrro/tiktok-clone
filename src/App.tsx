0047842127e1f20d8481221195b629ac2e32403b
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-01-25 15:35:45 -0300

    feat(Root): show video

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import { VideoPlayer } from "./components/VideoPlayer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,7 +8,8 @@ function App() {
   return (
     <div className="App">
       <main>
-        Tik Tok 
+        Tik Tok
+        <VideoPlayer />
       </main>
     </div>
   );
