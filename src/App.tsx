a0788422d4d4591ed39174ad2271c01167af6b04
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-01-25 13:17:13 -0300

    feat(Root): Start tiktok clone  project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <main>
+        Tik Tok 
+      </main>
+    </div>
+  );
+}
+
+export default App;
