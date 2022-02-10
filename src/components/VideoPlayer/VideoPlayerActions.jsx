15ff4294e31f4b429bb1fa5913f1f90f5493cae7
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-02-09 23:50:21 -0300

    fix: added actions button

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,19 +1,41 @@
 import React from "react";
-import { Heart } from "../../../assets/icons/Heart";
+import { Heart, Share, Comment } from "../../../assets/icons";
 import styles from "./styles.module.css";
 
-export const VideoPlayerActions = () => {
+export const VideoPlayerActions = ({
+  likes = 12,
+  comments = 22,
+  shares = 31,
+  hearted = false,
+}) => {
+  const handleLike = () => {
+    window.alert("You liked this video");
+  };
+
+  const handleComment = () => {
+    window.alert("You commented this video");
+  };
+
+  const handleShare = () => {
+    window.alert("You shared this video");
+  };
+
   return (
     <aside className={styles.actions}>
-      <div className={styles.action}>
-        <Heart />
-      </div>
-      <div className={styles.action}>
-        <Heart />
-      </div>
-      <div className={styles.action}>
-        <Heart />
-      </div>
+      <button className={styles.action} onClick={handleLike}>
+        <Heart witdh={45} />
+        <span title="Likes">{likes}</span>
+      </button>
+
+      <button className={styles.action} onClick={handleComment}>
+        <Comment witdh={45} />
+        <span title="comments">{comments}</span>
+      </button>
+
+      <button className={styles.action} onClick={handleShare}>
+        <Share witdh={45} />
+        <span title="shares">{shares}</span>
+      </button>
     </aside>
   );
 };
